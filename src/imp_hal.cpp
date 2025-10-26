ec13c9925109df4daadaf6897be90dc8901984e0
Paul Philippov <themactep@gmail.com>
2025-10-26 03:00:11 -0400

    Fix all escaped exclamation marks in preprocessor directives

--- src/imp_hal.cpp
+++ src/imp_hal.cpp
@@ -69,13 +69,13 @@ static PlatformCaps g_caps = {
 #endif
 
     // ISP capabilities
-#if \!defined(PLATFORM_T21) && \!defined(PLATFORM_T40) && \!defined(PLATFORM_T41)
+#if !defined(PLATFORM_T21) && !defined(PLATFORM_T40) && !defined(PLATFORM_T41)
     .has_isp_sinter = true,
 #else
     .has_isp_sinter = false,
 #endif
 
-#if \!defined(PLATFORM_T40) && \!defined(PLATFORM_T41)
+#if !defined(PLATFORM_T40) && !defined(PLATFORM_T41)
     .has_isp_temper = true,
 #else
     .has_isp_temper = false,
@@ -105,13 +105,13 @@ static PlatformCaps g_caps = {
     .has_isp_highlight_depress = false,
 #endif
 
-#if \!defined(PLATFORM_T21) && \!defined(PLATFORM_T40) && \!defined(PLATFORM_T41)
+#if !defined(PLATFORM_T21) && !defined(PLATFORM_T40) && !defined(PLATFORM_T41)
     .has_isp_ae_comp = true,
 #else
     .has_isp_ae_comp = false,
 #endif
 
-#if \!defined(PLATFORM_T40) && \!defined(PLATFORM_T41)
+#if !defined(PLATFORM_T40) && !defined(PLATFORM_T41)
     .has_isp_max_gain = true,
 #else
     .has_isp_max_gain = false,
